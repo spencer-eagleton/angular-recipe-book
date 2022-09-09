c22d26ce1f3823b743ef67c60cf9b00df9a52a68
Spencer Eagleton <seagleton@basspro.com>
2022-09-09 15:39:55 -0500

    file structure, created components

---
+++ src/app/Recipes/recipes.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'recipes-component',
+  templateUrl: './recipes.component.html'
+})
+export class RecipesComponent {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './Recipes/Recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './Recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './Recipes/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './Recipes/recipe-list/recipe/recipe.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeComponent,
+    ShoppingListEditComponent,
+    IngredientComponent,
   ],
   imports: [
     BrowserModule

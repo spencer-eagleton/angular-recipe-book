c971d47598bd088911e7814e9e1f0e4a9c57e1a1
Spencer Eagleton <eagletonspencer@gmail.com>
2022-09-20 11:34:06 -0500

    committing

--- src/app/Recipes/recipe-list/recipe-list.component.ts
+++ src/app/Recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'this is a test', 'https://static.onecms.io/wp-content/uploads/sites/44/2019/08/26231113/5783153.jpg' )
+  ];
 
   constructor() { }
 

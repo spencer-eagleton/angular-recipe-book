e68d32e284605f750b167f4aa7379624e9a192ca
Spencer Eagleton <eagletonspencer@gmail.com>
2022-09-30 10:11:33 -0500

    added routes for id/edit and new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import {RecipesComponent} from "./Recipes/Recipes.component";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 import {RecipeStartComponent} from "./Recipes/recipe-start/recipe-start.component";
 import {RecipeDetailComponent} from "./Recipes/recipe-detail/recipe-detail.component";
+import {RecipeEditComponent} from "./Recipes/recipe-edit/recipe-edit.component";
 
 const appRoutes : Routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent, children: [
-      {path: '', component: RecipeStartComponent },
-      {path: ':id', component: RecipeDetailComponent }
+      { path: '', component: RecipeStartComponent },
+      { path : 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      { path : ':id/edit', component: RecipeEditComponent }
     ]},
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { IngredientComponent } from './shopping-list/ingredient/ingredient.compo
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
 import { RecipeStartComponent } from './Recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './Recipes/recipe-edit/recipe-edit.component';
 
 
 
@@ -29,6 +30,7 @@ import { RecipeStartComponent } from './Recipes/recipe-start/recipe-start.compon
     ShoppingListEditComponent,
     IngredientComponent,
     RecipeStartComponent,
+    RecipeEditComponent,
   ],
   imports: [
     BrowserModule,

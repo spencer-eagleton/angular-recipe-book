542930d238ddb5ed73c310752908769ea6a9bbd5
Spencer Eagleton <eagletonspencer@gmail.com>
2022-10-13 11:58:43 -0500

    delete button deletes loaded item and is only enabled in Edit mode

--- src/app/Recipes/recipe.service.ts
+++ src/app/Recipes/recipe.service.ts
@@ -6,6 +6,7 @@ import {Subject} from "rxjs";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe('Test Recipe 1', 'this is a test', 'https://static.onecms.io/wp-content/uploads/sites/44/2019/08/26231113/5783153.jpg', [new Ingredient('Ingredient1', 1), new Ingredient('Ingredient2', 20),] ),
@@ -24,5 +25,15 @@ export class RecipeService {
   }
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
